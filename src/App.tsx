0aa8ab05ff038904d30baba84146acc1e8548e4b
ioofy <mynev.id@gmail.com>
2021-12-03 15:02:12 +0700

    feat: update a app.tsx for test ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Testing with github CI/CD Actions
         </p>
         <a
           className="App-link"
